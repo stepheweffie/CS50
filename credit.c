6f09914b1d3883175123f681da1e02b4985451b0
wtata <wtata@protonmail.com>
2019-05-15 00:00:33 -0400

    Refactor for test, hit dead end with double values

--- credit.c
+++ credit.c
@@ -6,22 +6,29 @@
 
 int main(void)
 {
-    //Run get_long on integer to determine check sum
+    //Run get_long on integer to determine checksum
     char card = get_long("Number: ");
     printf("%d\n", card);
+    // Check for prime number
     if (card %2 == 1)
     {
-        if (card %3 >= 1 && card %5 >= 1 && card %7 >= 1 && card %9 >= 1)
+        if (card %3 >= 1 && card %5 >= 1 && card %7 >= 1)
+        {
+            printf("MASTERCARD\n");
+        }
+    }
+    // Check for factor of 11
+    if (card %11 == 0)
         {
             printf("INVALID\n");
         }
-    }
     if (card < 0)
     {
         card *= -1;
-        //Check for even and product of 3 
+
+        //Check for even and product of 3
         if (card %3 == 0)
-        {    
+        {
             card /= 3;
             if (card %2 == 0)
             {
@@ -29,27 +36,23 @@ int main(void)
             }
             //Check for prime factor and factor of 3
             if (card %2 >= 1 && card %3 >= 1 && card %5 >= 1 && card %7 >= 1 && card %9 >= 1)
-            {    
+            {
                 printf("VISA\n");
             }
             else
             {
-                printf("INVALID\n"); 
-            }
-        }
-        if (card %2 == 1)
-        {
-            if (card %3 >= 1 && card %5 >= 1 && card %7 >= 1 && card %9 >= 1)
-            {
                 printf("INVALID\n");
             }
         }
-        else if (card %4 == 0)
-        {
-            printf("MASTERCARD\n");
-        }
+            else if (card %2 == 1)
+            {
+                if (card %3 >= 1 && card %5 >= 1 && card %7 >= 1 && card %9 >= 1)
+                {
+                    printf("INVALID\n");
+                }
+            }
     }
-    
+
     else if (card %4 == 0 && card > 0)
     {
         printf("INVALID\n");
@@ -64,7 +67,7 @@ int main(void)
         if (card %3 == 0)
         {
             do
-            { 
+            {
                 card /= 3;
                 printf("Dividing by 3\n");
             }
@@ -78,7 +81,7 @@ int main(void)
             }
             if (card %3 >= 1 && card %5 >= 1 && card %7 >= 1 && card %9 >= 1)
             {
-                printf("INVALID\n");        
+                printf("INVALID\n");
             }
         }
     }
