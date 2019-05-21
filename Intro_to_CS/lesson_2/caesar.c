99a05f6a73c51911c28dedab544c081b6030c7f0
wtata <wtata@protonmail.com>
2019-05-21 11:16:21 -0400

    if conditional, break, if/else

--- Intro_to_CS/lesson_2/caesar.c
+++ Intro_to_CS/lesson_2/caesar.c
@@ -1,73 +1,102 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 
 int main(int argc, string argv[])
 {
-    //Lowercase is difference of +32 uppercase
-    string pt = get_string("plaintext: ");
-    //char first = pt[0];
-    int len = strlen(pt);
-    int end = len - 1;
-    int key = atoi(argv[1]);
-    //key += 1;
-    //printf("%s\n%i\n", argv[1], key);
-    int letter;
-    int cindex;
-    //char* ctext[len];
-    //printf("%d\n", len);
-    char* uppers[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-                       "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
-    char* lowers[26] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
-                       "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-    printf("ciphertext: ");
-
-    for (int i = 0; i <= end ; i++)
+    // Check for usage of argv[1]
+    //printf("%s\n", argv[1]);
+    char* input = argv[1];
+    int i_input = strlen(argv[1]);
+    bool broken = true;
+    if (broken)
     {
-        // Encrypt lowercase
-        if (pt[i] <= 122 && pt[i] >= 96)
+    //printf("broken\n");
+
+        for (int ii = 0; ii < i_input; ii++)
         {
-            //printf("%d %d ", pt[i], i);
-            cindex = 96 - key;
-            letter = pt[i] - cindex;
-            printf("%d", letter);
-            if (letter > 25)
+            //printf("broken cont'd\n");
+            if (isspace(argv[1][ii]) || isalpha(argv[1][ii]))
             {
-                do
-                {
-                    letter %= 26;
-                    printf(" %d ", letter);
-                }
-                while (letter > 25);
+                printf("Usage: ./caesar key\n");
+                //broken = false;
+                // Out of loop after one print statement
+                break;
             }
-            //ctext[i] = lowers[letter];
-            letter -= 1;
-            printf("%s %i ", lowers[letter], letter);
-        }
-        // Encrypt uppercase
-        else if (pt[i] <= 90 && pt[i] >= 64)
-        {
-            //printf("b\n");
-            cindex = 64 - key;
-            letter = pt[i] - cindex;
-            if (letter > 25)
+
+            else
             {
-                do
+                //Lowercase is difference of +32 uppercase ASCII
+                string pt = get_string("plaintext: ");
+                //char first = pt[0];
+                int len = strlen(pt);
+                int end = len - 1;
+                int key = atoi(argv[1]);
+                //key += 1;
+                //printf("%s\n%i\n", argv[1], key);
+                int letter;
+                int cindex;
+                //char* ctext[len];
+                //printf("%d\n", len);
+
+                char* uppers[26] = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                                   "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+                char* lowers[26] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+                                   "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+                printf("ciphertext: ");
+
+                for (int i = 0; i <= end ; i++)
                 {
-                    letter %= 26;
+                    // Encrypt lowercase
+                    if (pt[i] <= 122 && pt[i] >= 96)
+                    {
+                        //printf("%d %d ", pt[i], i);
+                        cindex = 96 - key;
+                        letter = pt[i] - cindex;
+                        //printf("%d", letter);
+                        if (letter > 25)
+                        {
+                            do
+                            {
+                                letter %= 26;
+                                //printf(" %d ", letter);
+                            }
+                            while (letter > 25);
+                        }
+                        //ctext[i] = lowers[letter];
+                        letter -= 1;
+                        printf("%s", lowers[letter]);
+                    }
+                    // Encrypt uppercase
+                    else if (pt[i] <= 90 && pt[i] >= 64)
+                    {
+                        //printf("b\n");
+                        cindex = 64 - key;
+                        letter = pt[i] - cindex;
+                        if (letter > 25)
+                        {
+                            do
+                            {
+                                letter %= 26;
+                            }
+                            while (letter > 25);
+                        }
+                        //ctext[i] = uppers[letter];
+                        letter -= 1;
+                        printf("%s", uppers[letter]);
+                    }
+                    // Print other chars as they are
+                    else
+                    {
+                        printf("%c", pt[i]);
+                    }
                 }
-                while (letter > 25);
+                printf("\n");
+                broken = false;
+                break;
             }
-            //ctext[i] = uppers[letter];
-            letter -= 1;
-            printf("%s", uppers[letter]);
-        }
-        // Print other chars as they are
-        else
-        {
-            printf("%c", pt[i]);
         }
     }
-    printf("\n");
 }
